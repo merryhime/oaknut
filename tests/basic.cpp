e27d2f0306ecfb05ac4ecf96916bfceeab5bd812
Merry <git@mary.rs>
2022-07-09 20:14:06 +0100

    Implement CodeBlock

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -5,32 +5,26 @@
 #include <cstdio>
 
 #include <catch2/catch_test_macros.hpp>
-#include <libkern/OSCacheControl.h>
-#include <pthread.h>
-#include <sys/mman.h>
-#include <unistd.h>
 
+#include "oaknut/code_block.hpp"
 #include "oaknut/oaknut.hpp"
 
 TEST_CASE("Basic Test")
 {
-    const size_t page_size = getpagesize();
-    std::printf("page size: %zu\n", page_size);
-
-    std::uint32_t* mem = (std::uint32_t*)mmap(nullptr, page_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_JIT | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-
-    pthread_jit_write_protect_np(false);
-
     using namespace oaknut;
     using namespace oaknut::util;
 
-    CodeGenerator code{mem};
+    CodeBlock mem{4096};
+    CodeGenerator code{mem.ptr()};
+
+    mem.unprotect();
+
     code.MOVZ(W0, 42);
     code.RET(X30);
 
-    pthread_jit_write_protect_np(true);
-    sys_icache_invalidate(mem, page_size);
+    mem.protect();
+    mem.invalidate_all();
 
-    int result = ((int (*)())mem)();
+    int result = ((int (*)())mem.ptr())();
     REQUIRE(result == 42);
 }
