f7b0a1afa74159acc68d96fe586d3db60063fe59
Wunkolo <Wunkolo@gmail.com>
2023-10-03 21:56:14 -0700

    oaknut: Add LRCPC instructions
    These are only the LRCPC instructions(`LDAPR*`) and not LRCPC2 or
    LRCPC3.

--- include/oaknut/impl/mnemonics_generic_v8.3.inc.hpp
+++ include/oaknut/impl/mnemonics_generic_v8.3.inc.hpp
@@ -1,2 +1,19 @@
 // SPDX-FileCopyrightText: Copyright (c) 2023 merryhime <https://mary.rs>
 // SPDX-License-Identifier: MIT
+
+void LDAPR(WReg wt, XRegSp xn)
+{
+    emit<"1011100010111111110000nnnnnttttt", "t", "n">(wt, xn);
+}
+void LDAPR(XReg xt, XRegSp xn)
+{
+    emit<"1111100010111111110000nnnnnttttt", "t", "n">(xt, xn);
+}
+void LDAPRB(WReg wt, XRegSp xn)
+{
+    emit<"0011100010111111110000nnnnnttttt", "t", "n">(wt, xn);
+}
+void LDAPRH(WReg wt, XRegSp xn)
+{
+    emit<"0111100010111111110000nnnnnttttt", "t", "n">(wt, xn);
+}
