74ab28da3279f10825ad8178717c4ba655554dc6
Wunkolo <Wunkolo@gmail.com>
2023-10-03 17:27:19 -0700

    oaknut: Add FJCVTZS instruction

--- include/oaknut/impl/mnemonics_fpsimd_v8.3.inc.hpp
+++ include/oaknut/impl/mnemonics_fpsimd_v8.3.inc.hpp
@@ -1,2 +1,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2023 merryhime <https://mary.rs>
 // SPDX-License-Identifier: MIT
+
+void FJCVTZS(WReg wd, DReg rn)
+{
+    emit<"0001111001111110000000nnnnnddddd", "d", "n">(wd, rn);
+}
